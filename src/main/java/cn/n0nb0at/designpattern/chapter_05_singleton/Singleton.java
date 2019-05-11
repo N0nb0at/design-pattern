b3eb405de82164aed988f17526f9cddd07697614
guopeng <guopeng@hualala.com>
2019-05-11 17:58:42 +0800

    增加单例模式注释

--- src/main/java/cn/n0nb0at/designpattern/chapter_05_singleton/Singleton.java
+++ src/main/java/cn/n0nb0at/designpattern/chapter_05_singleton/Singleton.java
@@ -1,14 +1,29 @@
 package cn.n0nb0at.designpattern.chapter_05_singleton;
 
 /**
+ * 单例模式类
+ *
  * @author guopeng
  * @date 2019-05-11
  */
 public class Singleton {
+    /**
+     * 需要生成的单例
+     */
     private static Singleton singleton = new Singleton();
+
+    /**
+     * 构造方法
+     */
     private Singleton () {
         System.out.println("生成了一个实例。");
     }
+
+    /**
+     * 获取单例实例
+     *
+     * @return 单例实例
+     */
     public static Singleton getInstance() {
         return singleton;
     }
